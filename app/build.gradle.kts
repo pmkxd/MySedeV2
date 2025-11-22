7579f1a976abbc6597a60e223b04d15d31483d28
Pedro <pmkxd110@gmail.com>
2025-11-22 18:35:48 -0300

    cambio de version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.test.mysede"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
